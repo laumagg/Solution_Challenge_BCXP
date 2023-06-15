74505bfd601aa304f793a3baab55bb7ed992d38c
Laura Amaro <laura.magdalenoamaro@student.htw-berlin.de>
2023-06-15 23:34:41 +0200

    Implemented weather exercise
    - Created parser for weather data
    - parser call for weather
    - implementation of determination of smallest temp. spread

--- src/main/java/de/bcxp/challenge/App.java
+++ src/main/java/de/bcxp/challenge/App.java
@@ -1,38 +1,68 @@
 package de.bcxp.challenge;
 
 import de.bcxp.solution.CsvReader;
+import de.bcxp.solution.DataParser;
+import de.bcxp.solution.Weather;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public final class App {
-    private static void outputMinTemperatureSpread(){
-        // Read File
-        CsvReader reader = new CsvReader();
-        try {
-            reader.read(new File("weather.csv"), ',');
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        // TODO
-        //  Convert data
-        //  Output
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
-    }
-
-    private static void outputMaxPopulationDensity(){
-
-        // TODO
-        //  read file
-        //  convert
-        //  output
-        String countryWithHighestPopulationDensity = "Some country"; // Your population density analysis function call …
-        System.out.printf("Country with highest population density: %s%n", countryWithHighestPopulationDensity);
-    }
 
     public static void main(String[] args) {
 
         outputMinTemperatureSpread();
         outputMaxPopulationDensity();
+    }
+
+    private static List<String[]> readFile(String path, char separator) {
+        List<String[]> data = null;
+        CsvReader reader = new CsvReader();
+        try {
+            data = reader.read(new File(path), separator);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return data;
+    }
+
+    private static void outputMinTemperatureSpread() {
+        // Read File
+        List<String[]> data = readFile("src/main/resources/de/bcxp/challenge/weather.csv", ',');
+        if (data != null) {
+            // Parse data
+            DataParser parser = new DataParser();
+            List<Weather> weatherList = parser.convertToWeatherType(data);
+
+            //Check tempearture spread of every day
+            int dayWithLowestTempSpread = 0;
+            int smallestTempSpread = weatherList.get(0).getTemperatureSpread();
+            int currentTempSpread;
+
+            for (Weather weather : weatherList) {
+                currentTempSpread = weather.getTemperatureSpread();
+                //the smallest temp. spread is overwritten when the current temp. spread is smaller than its last value
+                if (currentTempSpread < smallestTempSpread) {
+                    smallestTempSpread = currentTempSpread;
+                    dayWithLowestTempSpread = weather.getDay();
+                }
+            }
+
+            System.out.printf("Day with smallest temperature spread: %s%n", dayWithLowestTempSpread);
+        }
+    }
+
+    private static void outputMaxPopulationDensity() {
+        // Read File
+        List<String[]> data = readFile("src/main/resources/de/bcxp/challenge/countries.csv", ';');
+        if (data != null) {
+            // TODO
+            //  read file
+            //  convert
+            //  output
+            String countryWithHighestPopulationDensity = "Some country"; // Your population density analysis function call …
+            System.out.printf("Country with highest population density: %s%n", countryWithHighestPopulationDensity);
+        }
     }
 }
--- src/main/java/de/bcxp/solution/DataParser.java
+++ src/main/java/de/bcxp/solution/DataParser.java
@@ -1,18 +1,36 @@
 package de.bcxp.solution;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataParser {
-    public List<Weather> convertToWeatherType(List<String[]> data){
-        List<Weather> convertedData = null;
-        //TODO
-        // check data compatibility
-        // create weather list
+    public List<Weather> convertToWeatherType(List<String[]> data) {
+        List<Weather> convertedData = new ArrayList<>();
 
+        int day, maxTemp, minTemp;
+        for (int i = 1; i < data.size(); i++) { //First line is title - will be ignored
+            try {
+                day = tryParseStringToInt(data.get(i)[0]);
+                maxTemp = tryParseStringToInt(data.get(i)[1]);
+                minTemp = tryParseStringToInt(data.get(i)[2]);
+
+                Weather weather = new Weather(day, maxTemp, minTemp);
+                convertedData.add(weather);
+            } catch (NumberFormatException e) {
+                System.out.printf("Error parsing CSV. Stopped at line: %s%n", i);
+            }
+        }
 
         return convertedData;
     }
 
+    private int tryParseStringToInt(String param) throws NumberFormatException {
+        try {
+            return Integer.parseInt(param);
+        } catch (NumberFormatException e) {
+            throw e;
+        }
+    }
 //TODO
 // create method for country data
 
